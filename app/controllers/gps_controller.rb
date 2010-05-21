1e5c65313c080d62be3500ee26c70c09ff849673
Pavel Chipiga <pavel.chipiga@gmail.com>
2010-05-21 21:34:25 +0300

    Improve distance display

--- app/controllers/gps_controller.rb
+++ app/controllers/gps_controller.rb
@@ -2,6 +2,8 @@ class GpsController < ApplicationController
   
   def index
     @red_stations= Station.with_color('red')
+    @green_stations= Station.with_color('green')
+    @blue_stations= Station.with_color('blue')
   end
   
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def distance(value)
+    "#{(value * 1000).round(2)} м"
+  end
+  
 end
