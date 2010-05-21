0edd0c827a1b7ad1c8da046e82a1e5d670e1c097
Vitaliy <vitalish@VitalishMacBookPro-2.local>
2010-05-21 21:09:41 +0300

    station

--- app/controllers/gps_controller.rb
+++ app/controllers/gps_controller.rb
@@ -1,22 +1,5 @@
 class GpsController < ApplicationController
   
-  def new
-    @user_session = UserSession.new
-    r = 6371
-    point1 = { :lat => 50.50196343778538, :lng => 30.49954891204834 }
-    point2 = { :lat => 50.51222576184578, :lng => 30.49851894378662 }
-    lat1 = point1[:lat]*Math::PI/180
-    lon1 = point1[:lng]*Math::PI/180
-    lat2 = point2[:lat]*Math::PI/180
-    lon2 = point2[:lng]*Math::PI/180
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = (Math.sin(dlat/2)*Math.sin(dlat/2)) + Math.cos(lat1) * Math.cos(lat2) * (Math.sin(dlon/2)*Math.sin(dlon/2))
-    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-    d = r * c
-    render :text => d
-  end
-  
   def index
     
   end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,26 @@
 class Station < ActiveRecord::Base
   has_one :connected, :foreign_key => 'connected_to', :primary_key => 'identifier', :class_name => 'Station'
+  
+  def distance_to_next
+    if next_station 
+      Station.distance(self,next_station) 
+    end
+  end
+  
+  def next_station
+    @next_station ||= Station.first(:conditions => {:color => self.color, :id => self.id+1})
+  end
+
+  def self.distance(point1,point2)
+    r = 6371
+    lat1 = point1[:lat]*Math::PI/180
+    lon1 = point1[:lng]*Math::PI/180
+    lat2 = point2[:lat]*Math::PI/180
+    lon2 = point2[:lng]*Math::PI/180
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = (Math.sin(dlat/2)*Math.sin(dlat/2)) + Math.cos(lat1) * Math.cos(lat2) * (Math.sin(dlon/2)*Math.sin(dlon/2))
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    d = r * c
+  end
 end
